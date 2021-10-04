a66c67976de81a4224e273e2c4fd410a008335aa
shivraj2806 <shivjadhav2806@gmail.com>
2021-10-04 20:13:02 +0530

    Adding: Adding data in BST

--- src/com/bridgelabz/BinarySearchTree_main.java
+++ src/com/bridgelabz/BinarySearchTree_main.java
@@ -2,15 +2,26 @@ package com.bridgelabz;
 
 public class BinarySearchTree_main {
     public static void main(String[] args) {
+        // "Welcome to Binary Search Tree Program"
+
         BinarySearchTree bst = new BinarySearchTree();
         Node root = null;
 
         root = bst.insert(root, 56);
         root = bst.insert(root, 30);
         root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
 
         bst.print(root);
+
     }
 }
-
-
